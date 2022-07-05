06eb1ef73f2a0aacb4416fcb7e8ec59adba1627f
Renato Pacheco <renato.pacheco@outlook.com>
2022-07-04 22:35:53 -0300

    Alterando banco

--- sql/Estrutura.sql
+++ sql/Estrutura.sql
@@ -1,20 +1,20 @@
 USE [Testando]
 GO
-/****** Object:  Table [dbo].[Usuario]    Script Date: 26/06/2022 19:03:57 ******/
+/****** Object:  Table [dbo].[Usuario]    Script Date: 04/07/2022 22:35:07 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Usuario](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Nome] [varchar](255) NULL,
-	[Email] [varchar](255) NULL,
-	[CriadoEm] [datetime] NULL,
-	[AlteradoEm] [datetime] NULL,
-	[Status] [varchar](50) NULL,
+	[Codigo_Usuario] [int] IDENTITY(1,1) NOT NULL,
+	[Nome_Usuario] [varchar](255) NULL,
+	[Email_Usuario] [varchar](255) NULL,
+	[DataCriacao_Usuario] [datetime] NULL,
+	[DataAlteracao_Usuario] [datetime] NULL,
+	[Status_Usuario] [char](1) NULL,
  CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
+	[Codigo_Usuario] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
