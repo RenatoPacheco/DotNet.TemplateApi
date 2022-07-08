c60617d2634e47bb9cae0a3c12d1cc07e2b33331
Renato Pacheco <renato.pacheco@adm.unip.br>
2022-07-08 18:42:21 -0300

    Criando recurso para validar enum como string

---
+++ src/Dominio/Validacoes/Notacoes/EnumForStringIsValidAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BitHelp.Core.Validation.Notations;
+
+namespace DotNetCore.API.Template.Dominio.Validacoes.Notacoes
+{
+    [AttributeUsage(AttributeTargets.Property |
+           AttributeTargets.Field, AllowMultiple = false)]
+    public class EnumForStringIsValidAttribute : ListIsValidAttribute
+    {
+        public EnumForStringIsValidAttribute(Type type) : base()
+        {
+            Type = type;
+        }
+
+        public Type Type { get; set; }
+
+        protected override bool Check(object value)
+        {
+            return Enum.IsDefined(Type, value.ToString());
+        }
+    }
+}
