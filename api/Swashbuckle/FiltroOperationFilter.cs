2b94dd3f20febb82241168edbf7c00db5c93abec
Renato Pacheco <renato.pacheco@outlook.com>
2022-07-13 15:05:01 -0300

    Com os comando passando pelo auto map, não preciso filtrar o json ignore

--- api/Swashbuckle/FiltroOperationFilter.cs
+++ api/Swashbuckle/FiltroOperationFilter.cs
@@ -1,8 +1,5 @@
-﻿using System.Linq;
-using Microsoft.OpenApi.Models;
-using System.Text.Json.Serialization;
+﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 
 namespace DotNetCore.API.Template.Site.Swashbuckle
 {
@@ -10,28 +7,7 @@ namespace DotNetCore.API.Template.Site.Swashbuckle
     {
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
-            context.ApiDescription.ParameterDescriptions
-               .Where(d => d.Source.Id == "Query").ToList()
-               .ForEach(param =>
-               {
-                   bool toIgnore =
-                       ((DefaultModelMetadata)param.ModelMetadata)
-                       .Attributes.PropertyAttributes
-                       ?.Any(x => x is JsonIgnoreAttribute) ?? false;
 
-                   if (toIgnore)
-                   {
-                       string aaa = param.Name;
-
-                       OpenApiParameter[] toRemove = operation.Parameters
-                           .Where(p => p.Name == param.Name || p.Name.StartsWith($"{param.Name}.")).ToArray();
-
-                       foreach (OpenApiParameter item in toRemove)
-                       {
-                           operation.Parameters.Remove(item);
-                       }
-                   }
-               });
         }
     }
 }
--- api/Swashbuckle/FiltroRequestBodyFilter.cs
+++ api/Swashbuckle/FiltroRequestBodyFilter.cs
@@ -1,8 +1,5 @@
-﻿using System.Linq;
-using Microsoft.OpenApi.Models;
-using System.Text.Json.Serialization;
+﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 
 namespace DotNetCore.API.Template.Site.Swashbuckle
 {
@@ -10,36 +7,7 @@ namespace DotNetCore.API.Template.Site.Swashbuckle
     {
         public void Apply(OpenApiRequestBody requestBody, RequestBodyFilterContext context)
         {
-            context?.FormParameterDescriptions
-                ?.Where(d => d.Source.Id == "Form").ToList()
-                .ForEach(param =>
-                {
-                    bool toIgnore =
-                       ((DefaultModelMetadata)param.ModelMetadata)
-                       .Attributes.PropertyAttributes
-                       ?.Any(x => x is JsonIgnoreAttribute) ?? false;
 
-                    if (toIgnore)
-                    {
-                        OpenApiMediaType openApiMediaType = requestBody.Content.SingleOrDefault(x => x.Key == "multipart/form-data").Value;
-
-                        if (!(openApiMediaType is null))
-                        {
-                            foreach (string item in openApiMediaType.Encoding.Keys.Where(
-                                x => x == param.Name || x.StartsWith($"{param.Name}.")))
-                            {
-                                openApiMediaType.Schema.Properties.Remove(item);
-                            }
-
-                            foreach (string item in openApiMediaType.Schema.Properties.Keys.Where(
-                                x => x == param.Name || x.StartsWith($"{param.Name}.")))
-                            {
-                                openApiMediaType.Encoding.Remove(item);
-                            }
-                        }
-
-                    }
-                });
         }
     }
 }
