827feea98b391930b1a5a22d099515bfc7f567fc
Renato Pacheco <renato.pacheco@outlook.com>
2022-08-15 10:25:33 -0300

    Configurando teste para a API

---
+++ test/Api/TestContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using TemplateApi.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace TemplateApi.Teste.Api
+{
+    public class TestContext
+    {
+        public TestContext()
+        {
+            _server = new TestServer(
+                new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseConfiguration(
+                    new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build())
+                .UseStartup<Startup>());
+            
+            Client = _server.CreateClient();
+        }
+
+        private readonly TestServer _server;
+        public HttpClient Client { get; private set; }
+    }
+}
