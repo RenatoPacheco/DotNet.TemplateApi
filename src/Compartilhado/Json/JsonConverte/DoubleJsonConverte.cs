911e8b871d89e0401f9de17f24ae04cc01c8dd4f
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-02-22 21:11:16 -0300

    Tratando ponto flutuante só quando for necessário

---
+++ src/Compartilhado/Json/JsonConverte/DoubleJsonConverte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+using TemplateApi.Compartilhado.ObjetosDeValor;
+
+namespace TemplateApi.Compartilhado.Json.JsonConverte
+{
+    public class DoubleJsonConverte
+        : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(double)
+                || objectType == typeof(double?);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            string value = reader?.Value?.ToString();
+            return value is null ? null : DoubleInput.TryParse(value, out DoubleInput v) ? (double)v : (double?)null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            DoubleInput v = value is double output ? (DoubleInput)output : null;
+
+            if (v is null)
+            {
+                writer.WriteNull();
+            }
+            else
+            {
+                if (v.ToString().IndexOf(".") >= 0)
+                {
+                    writer.WriteValue((double)v);
+                }
+                else
+                {
+                    writer.WriteRawValue(v?.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ src/Compartilhado/Json/JsonConverte/FloatJsonConverte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+using TemplateApi.Compartilhado.ObjetosDeValor;
+
+namespace TemplateApi.Compartilhado.Json.JsonConverte
+{
+    public class FloatJsonConverte
+        : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(float)
+                || objectType == typeof(float?);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            string value = reader?.Value?.ToString();
+            return value is null ? null : FloatInput.TryParse(value, out FloatInput v) ? (float)v : (float?)null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            FloatInput v = value is float output ? (FloatInput)output : null;
+
+            if (v is null)
+            {
+                writer.WriteNull();
+            }
+            else
+            {
+                if (v.ToString().IndexOf(".") >= 0)
+                {
+                    writer.WriteValue((float)v);
+                }
+                else
+                {
+                    writer.WriteRawValue(v?.ToString());
+                }
+            }
+        }
+    }
+}
