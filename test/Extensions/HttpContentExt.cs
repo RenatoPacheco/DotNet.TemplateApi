b2f2957f9e37a94c008fbde16bfb26bbe0355106
Renato Pacheco <renato.pacheco@adm.unip.br>
2023-11-14 09:37:26 -0300

    refactor: Obtendo dados de HttpContent com extensão simplificada

---
+++ test/Extensions/HttpContentExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateApi.Api.ViewsData;
+using System.Collections.Generic;
+using TemplateApi.Compartilhado.Json;
+
+namespace TemplateApi.Teste.Extensions
+{
+    public static class HttpContentExt
+    {
+        public static async Task<T> ReadAsObjectAsync<T>(this HttpContent content)
+        {
+            string json = await content.ReadAsStringAsync();
+            return ContratoJson.Desserializar<T>(json);
+        }
+
+        public static async Task<ComumViewData<T>> ReadAsViewDataAsync<T>(this HttpContent content)
+        {
+            return await content.ReadAsObjectAsync<ComumViewData<T>>();
+        }
+    }
+}
