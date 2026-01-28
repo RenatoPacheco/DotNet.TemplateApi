361c010a967386d211a7372651ec635dd3f5e2b8
Renato Pacheco <renato.pacheco@outlook.com>
2026-01-28 13:45:08 -0300

    feat: Atualizando projeto para core 8

--- api/App_Start/SwashbuckleConfig.cs
+++ api/App_Start/SwashbuckleConfig.cs
@@ -23,7 +23,7 @@ namespace TemplateApi.Api {
                     Title = AppSettings.Nome,
                     Version = "v1",
                     Description = $@"<p>Um projeto para montrar uma estrutura base de reursos e 
-                        configuraçãções para montar uma API em .Net Core 6.</p> 
+                        configuraçãções para montar uma API em .Net Core 8.</p> 
                         <p>Para mais informações consulte o projeto no <strong>Github</strong>, 
                         pelo link do website do swagger.</p>
                         <h3>Sobre a aplicação</h3>
